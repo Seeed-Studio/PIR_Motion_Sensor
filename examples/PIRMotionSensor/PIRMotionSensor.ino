4a0f21fed593fe49961145731cbff1738ddcd0f5
Jack Shao <jacky.shaoxg@gmail.com>
2014-08-01 11:40:25 +0800

    Update PIRMotionSensor.ino

--- examples/PIRMotionSensor/PIRMotionSensor.ino
+++ examples/PIRMotionSensor/PIRMotionSensor.ino
@@ -1,27 +1,40 @@
+/*
+ * PIRMotionSensor.ino
+ * Example sketch for the PIR motion sensor
+ *
+ * Copyright (c) 2013 seeed technology inc.
+ * Website    : www.seeed.cc
+ * Author     : FrankieChu
+ * Create Time: Jan 21,2013
+ * Change Log :
+ *
+ * The MIT License (MIT)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+ 
+ 
 /*****************************************************************************/
-//	Function: If the sensor detects the moving people in it's detecting range,
+//  Function: If the sensor detects the moving people in it's detecting range,
 //			  the Grove - LED is turned on.Otherwise, the LED is turned off.
 //  Hardware: Grove - PIR Motion Sensor, Grove - LED
 //	Arduino IDE: Arduino-1.0
-//	Author:	 Frankie.Chu		
-//	Date: 	 Jan 21,2013
-//	Version: v1.0
-//	by www.seeedstudio.com
-//
-//  This library is free software; you can redistribute it and/or
-//  modify it under the terms of the GNU Lesser General Public
-//  License as published by the Free Software Foundation; either
-//  version 2.1 of the License, or (at your option) any later version.
-//
-//  This library is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-//  Lesser General Public License for more details.
-//
-//  You should have received a copy of the GNU Lesser General Public
-//  License along with this library; if not, write to the Free Software
-//  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
-//
 /*******************************************************************************/
 /*macro definitions of PIR motion sensor pin and LED pin*/
 #define PIR_MOTION_SENSOR 2//Use pin 2 to receive the signal from the module 
