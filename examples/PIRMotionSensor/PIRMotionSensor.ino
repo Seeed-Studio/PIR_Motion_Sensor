448f3c88af96b3257fe9f7a6ee9eef74e433a939
improti <tim@dirasad.de>
2015-10-08 17:59:40 +0200

    Update PIRMotionSensor.ino
    corrections and cosmetic changes

--- examples/PIRMotionSensor/PIRMotionSensor.ino
+++ examples/PIRMotionSensor/PIRMotionSensor.ino
@@ -36,9 +36,9 @@
 //  Hardware: Grove - PIR Motion Sensor, Grove - LED
 //	Arduino IDE: Arduino-1.0
 /*******************************************************************************/
-/*macro definitions of PIR motion sensor pin and LED pin*/
-#define PIR_MOTION_SENSOR 2//Use pin 2 to receive the signal from the module 
-#define LED	4//the Grove - LED is connected to D4 of Arduino
+/* macro definitions of PIR motion sensor pin and LED pin */
+#define PIR_MOTION_SENSOR 2	//Use pin 2 to receive the signal from the module 
+#define LED	4	//the Grove - LED is connected to D4 of Arduino
 
 void setup()
 {
@@ -47,7 +47,7 @@ void setup()
 
 void loop() 
 {
-	if(isPeopleDetected())//if it detects the moving people?
+	if(isPeopleDetected()) //if it detects the moving people?
 		turnOnLED();
 	else
 		turnOffLED();
@@ -66,17 +66,17 @@ void turnOffLED()
 	digitalWrite(LED,LOW);
 }
 /***************************************************************/
-/*Function: Detect whether anyone moves in it's detecting range*/
-/*Return:-boolean, ture is someone detected.*/
+/* Function: Detect whether anyone moves in it's detecting range */
+/* Return:-boolean, true is someone detected.*/
 boolean isPeopleDetected()
 {
 	int sensorValue = digitalRead(PIR_MOTION_SENSOR);
-	if(sensorValue == HIGH)//if the sensor value is HIGH?
+	if(sensorValue == HIGH)	//if the sensor value is HIGH?
 	{
-		return true;//yes,return ture
+		return true;	//yes, return true
 	}
 	else
 	{
-		return false;//no,return false
+		return false;	//no, return false
 	}
 }
